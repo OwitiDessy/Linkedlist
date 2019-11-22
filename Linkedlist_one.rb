7ca63514fded7e1155f297c1b7e3e38286c8bb0d
Dessy Owiti <owitidessy@yahoo.com>
2019-11-22 10:17:52 +0000

    successfully completed linked list challenges 1 and 2

--- Linkedlist_one.rb
+++ Linkedlist_one.rb
@@ -17,7 +17,43 @@ def print_values(list_node)
   end
 end
 
+class Stack
+    attr_reader :data
+
+    def initialize
+        @data = nil
+    end
+
+    # Push a value onto the stack
+    def push(value)
+        # IMPLEMENT ME!
+        @data = LinkedListNode.new(value, @data)
+    end
+
+    # Pop an item off the stack.
+    # Remove the last item that was pushed onto the
+    # stack and return the value to the user
+    def pop
+        # I RETURN A VALUE
+        return print "nil\n" if @data.nil?
+        print "#{@data.value}\n"
+        @data = @data.next_node  
+    end
+
+end
+
 def reverse_list(list)
+  # ADD CODE HERE
+  stack = Stack.new
+
+    while list
+        # ADD CODE HERE
+        stack.push(list.value)
+        list = list.next_node
+    end
+
+    # ADD CODE HERE
+    return stack.data
 end
 
 node1 = LinkedListNode.new(37)
@@ -28,6 +64,6 @@ print_values(node3)
 
 puts "-------"
 
-#revlist = reverse_list(node3)
+revlist = reverse_list(node3)
 
-#print_values(revlist)
+print_values(revlist)
